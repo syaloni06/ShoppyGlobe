6b76493f78fd902e609f463914ccf85c3b035435
syaloni06 <syalonib@gmail.com>
2024-11-21 03:50:27 +0530

    fetch functionality added

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,7 +1,43 @@
-
-const ProductItem = () => {
+const ProductItem = (product) => {
   return (
-    <div>ProductItem</div>
+    <>
+   <div className="w-72 lg:w-60 bg-white m-5 flex flex-col rounded-lg shadow-lg hover:shadow-purple-800 hover:scale-105 overflow-hidden border-2 hover:border-purple-800 p-2">
+      {/* Product Image */}
+      <img
+        src={product.thumbnail} // Display the product's image
+        alt={product.title} // Alt text for the image
+        className="w-full h-56 object-cover" // Style the image
+      />
+
+      <div className="p-4">
+        {/* Product Title */}
+        <h1 className="text-lg mb-2.5 font-bold truncate">{product.title}</h1>
+
+        {/* Product Description */}
+        <p className="text-sm mb-2.5 text-gray-600 line-clamp-2">
+          {product.description}
+        </p>
+
+        {/* Product Price */}
+        <p className="text-base mb-2.5 text-green-600 font-bold">
+          ${product.price}
+        </p>
+
+        {/* Product Rating */}
+        <p className="flex gap-1 text-base mb-2.5 text-gray-600 font-semibold">
+          Rating: ⭐{product.rating}
+        </p>
+
+        {/* Button to view more details */}
+        <button
+          // onClick={() => handleViewDetails(product.id)}
+          className="text-sm text-purple-800 hover:underline decoration-2 font-bold"
+        >
+          View More Details
+        </button>
+      </div>
+    </div>
+  </>
   )
 }
 
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
+import useFetch from "../utils/useFetch";
+import ProductItem from "./ProductItem";
 
 const ProductList = () => {
+  const [productList, setProductList] = useState([]);
+  // function filterSearchList(filteredSearchBook){
+  //   setFilterBook(filteredSearchBook);
+  // }
+  const { data, error, loading } = useFetch(
+    "https://dummyjson.com/products?limit=50"
+  );
+  useEffect(() => {
+    if (data) {
+      setProductList(data.products);
+      console.log(data);
+    }
+  }, [data]);
+  if (error) {
+    return <p>Error in loading Data: {error}</p>;
+  }
+  if (loading) {
+    return <p> Loading...</p>;
+  }
   return (
-    <div>ProductList</div>
-  )
-}
+    <>
+      <div>
+        {productList.map((product) => {
+          <ProductItem product={product} key={product.id} />;
+        })}
+      </div>
+    </>
+  );
+};
 
 export default ProductList;
